15bc1ff8142866ad88f4e7d2d56ad050bdeb463c
lahh <raihanfazasabiqr@gmail.com>
2024-12-30 21:42:29 +0700

    adding function to upload file

--- backend/api/video_stream.go
+++ backend/api/video_stream.go
@@ -7,6 +7,8 @@ import (
 	"github.com/raihan-faza/lahhgrow/backend/responses"
 )
 
+const max_file_size = 1 << 30 //katanya ini 1gb
+
 func file_exist(file_name string) error {
 	_, err := os.Stat(file_name)
 	if err != nil {
@@ -22,5 +24,12 @@ func GetVideo(c *gin.Context, file_name string) {
 		return
 	}
 	responses.GoodRequest(c, file_name)
+	return
+}
+
+func UploadVideo(c *gin.Context, file_name string, path string) {
+	file, _ := c.FormFile("file")
+	c.SaveUploadedFile(file, path)
+	responses.GoodRequest(c, "file uploaded succesfully")
 	return
 }
