594028decd46d53d3cb44d990af39f1fb0d18911
lahh <raihanfazasabiqr@gmail.com>
2024-12-31 14:31:02 +0700

    adding course model and modified video model

--- backend/models/video.go
+++ backend/models/video.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type Video struct {
 	gorm.Model
-	Name string `json:"name"`
-	Path string `json:"path"`
-	Size int    `json:"size"`
+	Name     string `json:"name"`
+	Path     string `json:"path"`
+	Size     uint   `json:"size"`
+	CourseID uint   `json:"course_id"`
 }
