13dfa5e2ead76da91ef52601ae3ea46dfed2f912
lahh <raihanfazasabiqr@gmail.com>
2025-01-04 15:08:07 +0700

    updating jwt code generation code

--- backend/api/auth.go
+++ backend/api/auth.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/raihan-faza/lahhgrow/backend/utils"
 )
 
-func CreateToken(c *gin.Context, username string, user_id string, secret_key int) (string, string, error) {
+func CreateToken(c *gin.Context, username string, user_id string) (string, string, error) {
 	access_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userID":   user_id,
 		"username": username,
 		"exp":      time.Now().Add(15 * time.Minute).Unix(), //ini limabelasmenit
 	})
-	signed_access_token, err := access_token.SignedString(secret_key)
+	signed_access_token, err := access_token.SignedString([]byte(utils.SecretKey))
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +23,7 @@ func CreateToken(c *gin.Context, username string, user_id string, secret_key int
 		"username": username,
 		"exp":      time.Now().Add(7 * 24 * time.Hour).Unix(), //ini 7hari
 	})
-	signed_refresh_token, err := refresh_token.SignedString(secret_key)
+	signed_refresh_token, err := refresh_token.SignedString([]byte(utils.SecretKey))
 	if err != nil {
 		panic(err)
 	}
