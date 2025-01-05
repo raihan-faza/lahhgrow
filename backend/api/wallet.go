b44417ded513eb67d60129d74f16286e385ea9bf
lahh <raihanfazasabiqr@gmail.com>
2025-01-05 13:31:02 +0700

    init wallet api

---
+++ backend/api/wallet.go
@@ -0,0 +1,13 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+// nanti ku setup payment gatewaynya
+func AddBalance(c *gin.Context) {
+	return
+}
+
+// bisa ga ya pake cron buat nge cut per menit?
+func CutBalance(c *gin.Context) {
+	return
+}
