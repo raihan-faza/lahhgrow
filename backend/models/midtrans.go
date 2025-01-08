81510f84ccfec1c377605588dbada028af29856d
lahh <raihanfazasabiqr@gmail.com>
2025-01-08 21:06:29 +0700

    adding midtrans model

--- backend/models/midtrans.go
+++ backend/models/midtrans.go
@@ -1,13 +1,24 @@
 package models
 
-type MidtransRequest struct {
-	UserId   int    `json:"user_id" binding:"required"`
-	Amount   int64  `json:"amount" binding:"required"`
-	ItemID   string `json:"item_id" binding:"required"`
-	ItemName string `json:"item_name" binding:"required"`
+import "gorm.io/gorm"
+
+type TransactionDetails struct {
+	gorm.Model
+	OrderId           string
+	GrossAmount       string
+	CustomerDetailsID int
 }
 
-type MidtransResponse struct {
-	Token       string `json:"token"`
-	RedirectUrl string `json:"redirect_url"`
+type CustomerDetails struct {
+	gorm.Model
+	FName              string
+	LName              string
+	Phone              string
+	Address            string
+	City               string
+	Postcode           string
+	CountryCode        string
+	TransactionDetails []TransactionDetails
 }
+
+// i donno what am i doing smh
