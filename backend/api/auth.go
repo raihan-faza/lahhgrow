0a153bfc53c3eabf8bece9ce2996cc0d94240e78
lahh <raihanfazasabiqr@gmail.com>
2025-01-09 15:16:14 +0700

    adding function to decode jwt

--- backend/api/auth.go
+++ backend/api/auth.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -27,9 +28,8 @@ func CreateToken(c *gin.Context, db *gorm.DB, username string, password string, 
 		log.Fatal("somebody trying to do funny stuff")
 		return "", "", fmt.Errorf("hahaha")
 	}
-
 	access_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID":   user_id,
+		"user_id":  user_id,
 		"username": username,
 		"exp":      time.Now().Add(15 * time.Minute).Unix(), //ini limabelasmenit
 	})
@@ -38,7 +38,7 @@ func CreateToken(c *gin.Context, db *gorm.DB, username string, password string, 
 		panic(err)
 	}
 	refresh_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID":   user_id,
+		"user_id":  user_id,
 		"username": username,
 		"exp":      time.Now().Add(7 * 24 * time.Hour).Unix(), //ini 7hari
 	})
@@ -51,4 +51,34 @@ func CreateToken(c *gin.Context, db *gorm.DB, username string, password string, 
 
 func RefreshToken(c *gin.Context, refresh_token string) {
 	return
+}
+
+func DecodeJwt(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return utils.SecretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("invalid token")
+}
+
+func GetJwtFromHeader(c *gin.Context) string {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		log.Println("Authorization header not found")
+		return ""
+	}
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 {
+		log.Println("Invalid Authorization header format")
+		return ""
+	}
+	return parts[1]
 }
