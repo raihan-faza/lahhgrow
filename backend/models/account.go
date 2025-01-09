19a5d168709755df8c51c6600fa44954cff75fcd
lahh <raihanfazasabiqr@gmail.com>
2025-01-09 14:38:00 +0700

    changing account models

--- backend/models/account.go
+++ backend/models/account.go
@@ -4,7 +4,14 @@ import "gorm.io/gorm"
 
 type Account struct {
 	gorm.Model
-	Name     string `json:"name" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Wallet   Wallet
+	FirstName   string `json:"first_name" binding:"required"`
+	LastName    string `json:"last_name" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	Email       string `json:"email" binding:"required"`
+	Phone       string `json:"phone" binding:"required"`
+	Address     string `json:"address" binding:"required"`
+	City        string `json:"city" binding:"required"`
+	Postcode    string `json:"postcode" binding:"required"`
+	CountryCode string `json:"country_code" binding:"required"`
+	Wallet      Wallet
 }
