91ee658d28d3e809c6a5af870ae54dfe305af1fc
lahh <raihanfazasabiqr@gmail.com>
2025-01-10 21:41:26 +0700

    updating main.py

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from httpx import get, post
 
 app = Flask(__name__)
 
@@ -10,14 +11,46 @@ def index():
 
 @app.route("/course", methods=["GET"])
 def course():
+    data = get("http://localhost:8080/course")
     return render_template("course.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
-def register():
+def register(request):
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        address = request.form.get("address")
+        city = request.form.get("city")
+        postcode = request.form.get("postcode")
+        country_code = request.form.get("country_code")
+        json_data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "password": password,
+            "email": email,
+            "phone": phone,
+            "address": address,
+            "city": city,
+            "postcode": postcode,
+            "country_code": country_code,
+        }
+        try:
+            res = post("http://localhost:8080/register", json=json_data)
+            return redirect("/dashboard")
+        except:
+            return render_template("error.html")
     return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def user_login():
     return render_template("login.html")
+
+
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+    return render_template("dashboard.html")
