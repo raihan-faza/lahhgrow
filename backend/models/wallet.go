3d2083b0e41c6a088db91069b62ba4a742b854a7
lahh <raihanfazasabiqr@gmail.com>
2025-01-11 13:40:14 +0700

    adding wallet cron manager

--- backend/models/wallet.go
+++ backend/models/wallet.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 
+	"github.com/go-co-op/gocron/v2"
 	"gorm.io/gorm"
 )
 
@@ -16,10 +17,25 @@ type AddBalanceRequest struct {
 	Balance float64 `json:"balance" binding:"required"`
 }
 
+type WalletCronManager struct {
+	gorm.Model
+	Scheduler *gocron.Scheduler
+	WalletID  uint
+	Wallet    Wallet
+}
+
 func (w *Wallet) AddBalance(amount float64) error {
 	if amount < 0 {
 		return fmt.Errorf("you extorting me now?")
 	}
 	w.Balance = w.Balance + amount
+	return nil
+}
+
+func (w *WalletCronManager) CutBalance(amount float64) error {
+	if amount > w.Wallet.Balance {
+		return fmt.Errorf("you ran out of money")
+	}
+	w.Wallet.Balance = w.Wallet.Balance - amount
 	return nil
 }
